0c03c256511c165753e3514faa2c17416ca78401
Marco Godínez <markotom@gmail.com>
2013-09-04 13:20:23 -0500

    Configuración inicial de Backbone y Backbone Layout

--- public/app/main.js
+++ public/app/main.js
@@ -4,9 +4,21 @@ requirejs.config({
   baseUrl: 'app/',
   paths: {
     jquery: '../components/jquery/jquery.min',
-    bootstrap: '../components/bootstrap/dist/js/bootstrap.min'
+    bootstrap: '../components/bootstrap/dist/js/bootstrap.min',
+    underscore: '../components/underscore/underscore-min',
+    backbone: '../components/backbone/backbone-min',
+    layoutmanager: '../components/layoutmanager/backbone.layoutmanager',
   },
   shim: {
+    "*": ['backbone'],
+    backbone: {
+      deps: ['jquery', 'underscore'],
+      exports: 'Backbone'
+    },
+    layoutmanager: {
+      deps: ["backbone"],
+      exports: 'Backbone.Layout'
+    },
     bootstrap: {
       deps: ['jquery']
     },
