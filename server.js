03f70df4d25f6c3f3a91517242711e71c1cbf87e
Marco Godínez <markotom@gmail.com>
2013-09-06 02:42:20 -0500

    Se creó una ruta fake de REST para empezar a trabajar con Backbone

--- server.js
+++ server.js
@@ -41,4 +41,49 @@ requirejs(['config', 'express'], function(config, express){
     server.enable('view cache');
   });
 
+  server.get('/api/1.0/reports', function (req, res) {
+    res.send([
+      {
+        id: 1,
+        year: 2013,
+        title: 'Cursos',
+        type: 'Valoración',
+        topic: 'Cátedras',
+        contents: '...',
+        created_at: new Date(),
+        update_at: new Date()
+      },
+      {
+        id: 2,
+        year: 2013,
+        title: 'Cursos',
+        type: 'Número',
+        topic: 'Cátedras',
+        contents: '...',
+        created_at: new Date(),
+        update_at: new Date()
+      },
+      {
+        id: 3,
+        year: 2013,
+        title: 'Conferencias',
+        type: 'Valoración',
+        topic: 'Cátedras',
+        contents: '...',
+        created_at: new Date(),
+        update_at: new Date()
+      },
+      {
+        id: 4,
+        year: 2012,
+        title: 'Cursos',
+        type: 'Valoración',
+        topic: 'Cátedras',
+        contents: '...',
+        created_at: new Date(),
+        update_at: new Date()
+      },
+    ]);
+  });
+
 });
