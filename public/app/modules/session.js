523319749e907aea2bd118d13ad9abf0ea74d8f5
Marco Godínez <markotom@gmail.com>
2013-09-09 04:02:25 -0500

    Se creó el módulo de Session para la autentificación con cross domain
    Además se crea vistas básicas de logged in y log out

---
+++ public/app/modules/session.js
@@ -0,0 +1,71 @@
+'use strict';
+
+define(['app'], function (app) {
+
+  var Session = app.module();
+
+  Session.Model = Backbone.Model.extend({
+    idAttribute: 'user',
+    url: '/session',
+    login: function (credentials) {
+      this.save(credentials);
+    },
+    logout: function () {
+      var that = this;
+      this.destroy({
+        success: function (model, resp) {
+          model.clear();
+          model.id = null;
+
+          // Refresh Csrf Token
+          app.csrf = resp.csrf;
+
+          that.set({ auth: false, user: null });
+        }
+      });
+    }
+  });
+
+  Session.Views.Form = Backbone.View.extend({
+    scope: '#users',
+    initialize: function () {
+
+      // form out of main element
+      $(this.scope).html(this.el);
+
+      var that = this;
+
+      this.model.on('change:auth', function (session) {
+        that.render();
+      });
+
+    },
+    beforeRender: function () {
+      if (this.model.get('auth')) {
+        this.template = 'user/logout';
+      } else {
+        this.template = 'user/login';
+      }
+    },
+    events: {
+      'submit form': 'login',
+      'click .logout': 'logout'
+    },
+    login: function (e) {
+      e.preventDefault();
+
+      this.model.login({
+        email: $(this.el).find('#email').val(),
+        password: $(this.el).find('#password').val()
+      });
+
+    },
+    logout: function (e) {
+      e.preventDefault();
+      this.model.logout();
+    }
+  });
+
+  return Session;
+
+});
