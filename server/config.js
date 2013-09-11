03aeb9062ef915d2505726422a82dd486142fbdd
Marco Godínez <markotom@gmail.com>
2013-09-11 01:47:52 -0500

    Se añadió configuración para la conexión con mongodb

--- server/config.js
+++ server/config.js
@@ -11,7 +11,15 @@ define(['underscore'], function (_) {
     port: 3000
   };
 
-  development = _.extend(defaults, {});
+  development = _.extend(defaults, {
+    mongo: {
+      user: '',
+      pass: '',
+      host: 'localhost',
+      port: 27017,
+      db: 'informes'
+    }
+  });
   production  = _.extend(defaults, {});
 
   if ('production' === process.env.NODE_ENV) {
