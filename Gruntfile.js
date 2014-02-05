814d9181743820e812ea2909b734b5c00cef60b4
Marco Godínez <markotom@gmail.com>
2014-02-05 13:57:35 -0600

    Se creó tarea de Grunt para generar json de usuarios para importarlos a Mongo

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@
 
 module.exports = function (grunt) {
 
+  // crypto
+  var crypto = require('crypto');
+
   // load all grunt tasks
   require('load-grunt-tasks')(grunt);
 
@@ -139,6 +142,81 @@ module.exports = function (grunt) {
         commitMessage: 'Release <%= version %>',
         tagMessage: 'Version <%= version %>'
       }
+    },
+
+    prompt: {
+      users: {
+        options: {
+          questions: [
+            {
+              config: 'config.params.users.file',
+              type: 'input',
+              message: '¿Cuál es la ubicación del archivo?',
+              default: './tmp/sheet.json'
+            },
+            {
+              config: 'config.params.users.output',
+              type: 'input',
+              message: '¿Cuál es la ubicación del archivo por crear?',
+              default: './tmp/users.json'
+            },
+            {
+              config: 'config.params.users.admin.lastname',
+              type: 'input',
+              message: '¿Cuáles son los apellidos del administrador?',
+              default: 'Godínez Bustos'
+            },
+            {
+              config: 'config.params.users.admin.firstname',
+              type: 'input',
+              message: '¿Cuál es el nombre del administrador?',
+              default: 'Marco'
+            },
+            {
+              config: 'config.params.users.admin.email',
+              type: 'email',
+              message: '¿Cuál es el correo electrónico del administrador?',
+              default: 'mgodinez@filos.unam.mx'
+            },
+            {
+              config: 'config.params.users.admin.password',
+              type: 'password',
+              message: '¿Cuál es la contraseña del administrador?',
+              default: '12345'
+            },
+            {
+              config: 'config.params.users.fields.firstname',
+              type: 'input',
+              message: '¿Cuál es la llave del campo "Nombre"?',
+              default: 'Nombre'
+            },
+            {
+              config: 'config.params.users.fields.lastname',
+              type: 'input',
+              message: '¿Cuál es la llave del campo "Apellidos"?',
+              default: 'Apellidos'
+            },
+            {
+              config: 'config.params.users.fields.email',
+              type: 'input',
+              message: '¿Cuál es la llave del campo "Correo electrónico"?',
+              default: 'Correo-e'
+            },
+            {
+              config: 'config.params.users.fields.password',
+              type: 'input',
+              message: '¿Cuál es la llave del campo "Contraseña"?',
+              default: 'Contraseña'
+            },
+            {
+              config: 'config.params.users.fields.guidelines',
+              type: 'input',
+              message: '¿Cuál es la llave del campo "Guías"?',
+              default: 'Guías'
+            }
+          ]
+        }
+      },
     }
 
   });
@@ -162,6 +240,47 @@ module.exports = function (grunt) {
 
   });
 
+  grunt.registerTask('users', function () {
+
+    var params = grunt.config.get('config.params.users');
+
+    var sha = crypto.createHash('sha256');
+
+    var output  = [{
+      role: 'Administrator',
+      name: {
+        first: params.admin.firstname,
+        last: params.admin.lastname
+      },
+      email: params.admin.email,
+      password: sha.update(params.admin.password).digest('hex')
+    }];
+
+    if (grunt.file.isFile(params.file)) {
+      grunt.file.readJSON(params.file).forEach(function (user) {
+        
+        var sha = crypto.createHash('sha256');
+
+        output.push({
+          role: 'Reporter',
+          name: {
+            first: user[params.fields.firstname],
+            last: user[params.fields.lastname]
+          },
+          email: user[params.fields.email],
+          password: sha.update(user[params.fields.password]).digest('hex'),
+          guidelines: user[params.fields.guidelines]
+        });
+      });
+
+      grunt.file.write(params.output, JSON.stringify(output));
+    }
+  });
+
+  // Build users json file
+  grunt.registerTask('build:users', ['prompt:users', 'users']);
+
+  // Build production files
   grunt.registerTask('build', ['preprocess:prod', 'jst', 'requirejs']);
 
 };
